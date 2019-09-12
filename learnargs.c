ca03a0d590f9f3cb3d5058565091fb9c64303f4e
Jerry Pass <arflierl@buffalo.edu>
2019-09-11 21:37:32 -0400

    learning how command line arguments work in *argv[] and char *str etc.

--- learnargs.c
+++ learnargs.c
@@ -2,10 +2,10 @@
 #include <string.h>
 #include <stdbool.h>
 
-int parse(char *str);
-int printString(char *str);
+void parse(char *str);
+void printString(char *str);
 
-int main(int argc, char *argv[]) {
+void main(int argc, char *argv[]) {
   char a = *argv[1]; //pointer to the first character of argv[1]
   int b = *argv[1];  // pointer to the first chracter of argv[1], integer char value
   char *hello = argv[1]; //this is how you can access the entire array of chars of argv[1], the "string"
@@ -20,22 +20,19 @@ int main(int argc, char *argv[]) {
   parse(argv[1]);
   char num[] = "1234";
   printf("%d\n", atoi(num) - 1);
-  return 0;
-  
 }
 
-int printString(char *str) {
+void printString(char *str) {
   printf("%s\n", str);  // this function(char *str) --> char *str is from argv[i] in main from command line and is the array of chars, or "string".  See *hello in mian.
 }  
 
-int parse(char *str) {
+void parse(char *str) {
   for (int i = 0; str[i] != '\0'; i++) {
     printf("%c\n", str[i]);
   }
   for (int j = 0; str[j] != '\0'; j++) {
     printf("%d\n", str[j]);
   }  
-  return 0;
 }  
 
 
