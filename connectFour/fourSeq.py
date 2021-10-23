0e222f77cc779d861077c92abc93c07ac7272f7f
paolo-Rancati <52716043+paolo-Rancati@users.noreply.github.com>
2021-10-23 18:17:45 -0400

    Add files via upload

---
+++ connectFour/fourSeq.py
@@ -0,0 +1,118 @@
+import numpy as np
+import pygame
+import math
+
+YELLOW = (255,255,0)
+RED = (255,0,0)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+CERULEAN = (42,82,190)
+SQUARE = 100 # 100 pixels
+RADIUS = 45
+
+# this function simply creates the game board
+# it is only called once in main before the game begins
+def create_board():
+    board = np.zeros((6,7))
+    return board
+
+def display_board(screen: pygame.surface, board: np.ndarray):
+    for _col in range(7):
+        for _row in range(6):
+            pygame.draw.rect(screen, YELLOW, (_col*SQUARE, _row*SQUARE+SQUARE, SQUARE, SQUARE))
+            if board[_row][_col] == 0:
+                pygame.draw.circle(screen, WHITE, (int(_col*SQUARE+SQUARE/2), int(_row*SQUARE+SQUARE+SQUARE/2)),RADIUS)
+            elif board[_row][_col] == 1:
+                pygame.draw.circle(screen, BLACK, (int(_col*SQUARE+SQUARE/2), int(_row*SQUARE+SQUARE+SQUARE/2)),RADIUS)
+            else:
+                pygame.draw.circle(screen, RED, (int(_col*SQUARE+SQUARE/2), int(_row*SQUARE+SQUARE+SQUARE/2)),RADIUS)
+    pygame.draw.rect(screen, CERULEAN, (0, 0, SQUARE*7, SQUARE))
+    pygame.display.update()
+
+# this function requests and returns the user input
+# it asks the user for a column in which to drop the checker
+def playerInput(turn: int, column: int):
+    curPlayer = turn + 1
+    print(f"player{curPlayer} inserting at x-axis pixel {column}")
+    print("column/SQUARE", column/SQUARE)
+    return int(math.floor(column/SQUARE)) + 1
+# this function finds the first open row in the requested column
+# and inserts the player number into the first open slot
+# if the requested column is full, it alerts the user
+# and returns false, otherwise it returns true
+def dropPiece(player: int, column: int, board: np.ndarray):
+    if board[5][column - 1] == 0:
+        openRow = 0
+        while openRow < 6:
+            if board[openRow][column - 1] == 0:
+                board[openRow][column - 1] = player
+                return True
+            else:  openRow += 1
+    else:
+        print(f"player{player}, you have selected an invalid location")
+        return False
+
+def chickenDinner():
+    pygame.mixer.stop()
+    win = pygame.mixer.Sound("sounds/win.mp3")
+    win.play()
+    while pygame.mixer.get_busy():
+        pygame.time.delay(10)
+        pygame.event.poll()
+
+def winCheck(player: int, board: np.ndarray):
+    # check diagonals SW to NE
+    # because we need to have four in a sequence
+    # on diagonals we can only count rows 1 and 2 to get four
+    # from this direction, which is like the upper half
+    # of a diagonal slice if looking at real connect four
+    for _rowStart in range (0, 2):
+        count = 0
+        for _row, _column in zip(range(_rowStart,6), range(0,7)):
+            if board[_row][_column] == player:
+                count += 1
+                if count >= 4:
+                    return True
+            else: count = 0
+
+    for _colStart in range (1,4):
+        count = 0
+        for _row, _column in zip(range(0,6), range(_colStart,7)):
+            if board[_row][_column] == player:
+                count += 1
+                if count >= 4:
+                    return True
+            else: count = 0
+
+    #check horizontal
+    for _rowCheck in range(0,6):
+        count = 0
+        for _column in range(0,7):
+            if board[_rowCheck][_column] == player:
+                count += 1
+                if count >= 4:
+                    return True
+            else: count = 0
+
+    #check vertical
+    for _columnCheck in range(0,7):
+        count = 0
+        for _row in range(0,6):
+            if board[_row][_columnCheck] == player:
+                count += 1
+                if count >= 4:
+                    return True
+            else: count = 0
+
+    #negative sloping diagonals
+    for _col in range(4):
+        for _row in range(3, 6):
+            if board[_row][_col] == player and board[_row - 1][_col + 1] == player and board[_row - 2][_col + 2] and board[_row - 3][_col + 3] == player:
+                return True
+
+    return False
+
+
+
+
+
---
+++ connectFour/main.py
@@ -0,0 +1,92 @@
+import numpy as np
+import fourSeq
+import pygame
+import sys
+import random
+
+game_over = False
+turn = 0
+column = 0
+fullColumns = [0,0,0,0,0,0,0]
+gameplaySongSelectionList = ["sounds/play.mp3", "sounds/play2.mp3", "sounds/play3.mp3", "sounds/play4.mp3", "sounds/play5.mp3"]
+
+if __name__ == '__main__':
+    board = fourSeq.create_board()
+    pygame.init()
+    pygame.mixer.init()
+    width = 700 # this is for 7 columns, each with a width of 100 pixels
+    height = 700 # this is for 6 rows, each with a width of 100 pixels plus an additional row
+    size = (width, height)
+    screen = pygame.display.set_mode(size)
+    fourSeq.display_board(screen, board)
+    song = random.choice(gameplaySongSelectionList)
+    play = pygame.mixer.Sound(song)
+    pygame.mixer.Sound.play(play)
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.MOUSEMOTION:
+                pygame.draw.rect(screen, fourSeq.CERULEAN, (0,0,width,fourSeq.SQUARE))
+                x = event.pos[0]
+                if turn == 0:
+                    pygame.draw.circle(screen, fourSeq.BLACK, (x,50),fourSeq.RADIUS)
+                else:
+                    pygame.draw.circle(screen, fourSeq.RED, (x, 50), fourSeq.RADIUS)
+                pygame.display.update()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                dropFX = pygame.mixer.Sound("sounds/drop.mp3")
+                pygame.mixer.Sound.play(dropFX)
+                pygame.draw.rect(screen, fourSeq.CERULEAN, (0, 0, width, fourSeq.SQUARE))
+                dropHere = event.pos[0]
+                # Ask for player1 input
+                if turn == 0:
+                    column = fourSeq.playerInput(turn, dropHere)
+                    # make sure the column falls within correct range
+                    # although programmers start counting at zero
+                    # most of our users will start at one
+                    if column in range(1, 8):
+                        if fourSeq.dropPiece(turn + 1, column, board):
+                            turn += 1
+                            print(np.flip(board, 0))
+                            fourSeq.display_board(screen, np.flip(board,0))
+                            if fourSeq.winCheck(1, board):
+                                print(f"player{turn + 1} Wins!!!!!")
+                                fourSeq.chickenDinner()
+                                game_over = True
+                            if board[5][column - 1] != 0:
+                                fullColumns[column - 1] = column
+                            if fullColumns == [1, 2, 3, 4, 5, 6, 7]:
+                                print("DRAW!!!!!")
+                                game_over = True
+                    else:
+                        print(f"player{turn + 1} you chose an invalid column to place your piece")
+                else:
+                    # Ask for player2 input
+                    column = fourSeq.playerInput(turn, dropHere)
+                    # make sure the column falls within correct range
+                    # although programmers start counting at zero
+                    # most of our users will start at one
+                    if column in range(1, 8):
+                        if fourSeq.dropPiece(turn + 1, column, board):
+                            turn = 0
+                            print(np.flip(board, 0))
+                            fourSeq.display_board(screen, np.flip(board,0))
+                            if fourSeq.winCheck(2, board):
+                                print(f"player{turn + 1} Wins!!!!!")
+                                fourSeq.chickenDinner()
+                                game_over = True
+                            if board[5][column - 1] != 0:
+                                fullColumns[column - 1] = column
+                            if fullColumns == [1, 2, 3, 4, 5, 6, 7]:
+                                print("DRAW!!!!!")
+                                game_over = True
+                    else:
+                        print(f"player{turn + 1} you chose an invalid column to place your piece")
+
+
+
+
+
+
+
